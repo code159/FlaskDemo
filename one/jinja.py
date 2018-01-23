cf2a5ce60505e1e64616ae2ddee7e0f340da9ac2
code159 <409901403@qq.com>
2018-01-23 23:27:02 +0800

    新增bootstrap栅格系统

--- one/jinja.py
+++ one/jinja.py
@@ -1,19 +1,17 @@
 from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 
 app=Flask(__name__)
-
+bootstrap=Bootstrap(app)
 lists = [1,2,3]
 
 @app.route("/<name>")
 def index(name):
     return render_template("jinja_test.html", name=name, lists=lists)
 
-
-
 @app.route("/if")
 def ifdef():
     return render_template("jinja_test.html", lists=lists)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
