965b7fdaf2dfbaa8d3dcad6af238cb86b595e523
liyubin <lixin58688@163.com>
2018-03-23 15:42:36 +0800

    新增bootstrap相关内容

--- multiSimple/app/__init__.py
+++ multiSimple/app/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template 
 from flask_bootstrap import Bootstrap 
 from flask_sqlalchemy import SQLAlchemy 
-from config import config
+from ..config import config
 from flask_login import LoginManager
 from flask_mail import Mail
 
